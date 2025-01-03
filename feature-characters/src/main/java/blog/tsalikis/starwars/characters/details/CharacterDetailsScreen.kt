a6ae97a58273f44472a95130134cfab1d6aee689
Kostas Tsalikis <tsalikispk@gmail.com>
2025-01-04 00:05:30 +0200

    Replace deprecated Icon vector

--- feature-characters/src/main/java/blog/tsalikis/starwars/characters/details/CharacterDetailsScreen.kt
+++ feature-characters/src/main/java/blog/tsalikis/starwars/characters/details/CharacterDetailsScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -71,7 +71,7 @@ fun CharacterDetailsScreen(
         topBar = {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Icon(
-                    imageVector = Icons.Default.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = "Back",
                     modifier = Modifier.clickable { backDispatcher?.onBackPressed() }
                 )
