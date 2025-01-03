b7feac41e72f1e36128c602f0c55f471e2eab1c4
Kostas Tsalikis <tsalikispk@gmail.com>
2025-01-03 23:47:18 +0200

    Add slide in and out transition between list and details screens

--- app/src/main/java/blog/tsalikis/starwars/MainActivity.kt
+++ app/src/main/java/blog/tsalikis/starwars/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -16,6 +18,8 @@ import blog.tsalikis.starwars.characters.ui.characters
 import blog.tsalikis.starwars.design.theme.StarWarsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+private const val SLIDE_TRANSITION_MILLIS = 600
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +29,29 @@ class MainActivity : ComponentActivity() {
             StarWarsAppTheme {
                 Box(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
                     val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = CHARACTERS) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = CHARACTERS,
+                        enterTransition = {
+                            slideIntoContainer(
+                                AnimatedContentTransitionScope.SlideDirection.Start,
+                                tween(
+                                    SLIDE_TRANSITION_MILLIS
+                                )
+                            )
+                        },
+                        exitTransition = {
+                            slideOutOfContainer(
+                                AnimatedContentTransitionScope.SlideDirection.End,
+                                tween(
+                                    SLIDE_TRANSITION_MILLIS
+                                )
+                            )
+                        }
+                    ) {
                         characters(onDetails = { id, name ->
                             navController.navigate(
-                                "characterDetails/$id/$name",
+                                route = "characterDetails/$id/$name",
                             )
                         })
                         details()
