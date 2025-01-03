632c09df06b89ada728f1c4f248f3d0196e235cd
Kostas Tsalikis <tsalikispk@gmail.com>
2025-01-03 17:54:51 +0200

    Fix status bar padding after adding the NavHost

--- app/src/main/java/blog/tsalikis/starwars/MainActivity.kt
+++ app/src/main/java/blog/tsalikis/starwars/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import blog.tsalikis.starwars.characters.details.details
@@ -19,14 +23,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StarWarsAppTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = CHARACTERS) {
-                    characters(onDetails = { id, name ->
-                        navController.navigate(
-                            "characterDetails/$id/$name",
-                        )
-                    })
-                    details()
+                Box(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = CHARACTERS) {
+                        characters(onDetails = { id, name ->
+                            navController.navigate(
+                                "characterDetails/$id/$name",
+                            )
+                        })
+                        details()
+                    }
                 }
             }
         }
